00a96be11b398954f1b5d5ee64a351ee020a3cfd
Mohammed-Elsayed-Elmorsy <124476206+Mohammed-Elsayed-Elmorsy@users.noreply.github.com>
2023-07-01 00:07:14 -0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,15 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
+    next();
+})
 const cors = require('cors')
 app.use(express.static(__dirname));
-app.use(cors({ credentials: true, origin: ['http://localhost:3000', '*'] }))
+app.use(cors({ credentials: true, origin: ['http://localhost:3000', 'http://127.0.0.1:5500', '*'] }))
 const connectDb = require('./config/db')
 
 const router = require('./routes/Routes')
